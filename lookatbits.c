2258b5eacb2f39d3c358edc9033e8c08fc9a6683
Liam-Higgins2018 <liammhiggins@hotmail.com>
2020-02-19 19:48:33 +0000

    Lookey loo

--- lookatbits.c
+++ lookatbits.c
@@ -1,24 +1,17 @@
 	#include <stdio.h>
-	#include <stdint.h>
+	#include <inttypes.h>
 
+	void printbits(uint_t s){
+	for(int i = 63; i > 0; i--)
+	{
+		printf("%d",((1 << i) & s) ? 1 : 0 );
+	}
+}
 
 	int main(int argc, char *argv[]){
-		char c =65;
-		int i = 65;
-		long int l = 65;
-		long long int m = 65;
-
-		uint8_t u = 65;
 		uint64_t r = 65; 
-
-
-		printf("%x\t%c\t%d\t%d\n", c, c, sizeof(c;
-
-		printf("%x\t%c\t%d\t%d\n", i, i, sizeof(i));
-
-		printf("%x\t%c\t%d\t%d\n", l, l, sizeof(l));
-
-		printf("%x\t%c\t%d\t%d\n", m, m, sizeof(m));
+		printbits(s);
+		printf("%llx\t%lld\n", r, r);
 
 		return 0;
 
